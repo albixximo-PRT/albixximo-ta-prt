df233710ba7897b892e4e5405435a5ff057df9cf
Albixximo <albixximo@gmail.com>
2026-05-30 20:55:10 +0200

    focuss

--- app/albixximo/page.tsx
+++ app/albixximo/page.tsx
@@ -6394,9 +6394,9 @@ function resetBaselineDraft() {
         } else {
           const bestMatch = findBestOfficialPilotMatch(resolvedPilot, officialLeaguePilots)
 
-          if (bestMatch?.isSafeAutoMatch) {
-            resolvedPilot = bestMatch.officialName
-          } else if (normalizedRaw) {
+          if (bestMatch?.isSafeAutoMatch && bestMatch.score >= 0.96) {
+  resolvedPilot = bestMatch.officialName
+} else if (normalizedRaw) {
             const unresolvedId = `${selectedLeague}:${normalizedRaw}`
 
             if (!dismissedUnknownDrivers[unresolvedId] && !unresolvedMap.has(unresolvedId)) {
